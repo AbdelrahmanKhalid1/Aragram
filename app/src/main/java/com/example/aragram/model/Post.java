49d7470619d4ade36cec2d0434533f036d5fb671
rafeek <rafeeknehad333@gmail.com>
2020-11-06 13:58:36 +0200

    new commit

--- app/src/main/java/com/example/aragram/model/Post.java
+++ app/src/main/java/com/example/aragram/model/Post.java
@@ -1,5 +1,108 @@
 package com.example.aragram.model;
 
-public class Post {
+import com.google.firebase.database.Exclude;
 
+import java.util.List;
+
+public class Post {
+    private User userProfile;
+    private String postImage;
+    private boolean like;
+    private boolean save;
+    private List<User> likeList;
+    private List<PostComment> commentList;
+    private int time;
+    private String description;
+
+    @Exclude
+    public String getDocumentId() {
+        return documentId;
+    }
+
+    @Exclude
+    public void setDocumentId(String documentId) {
+        this.documentId = documentId;
+    }
+
+    @Exclude
+    private String documentId;
+
+    public Post() {
+    }
+
+    public Post(User userProfile, String postImage, boolean like, boolean save,
+                List<User> likeList, List<PostComment> commentList, int time, String description) {
+        this.userProfile = userProfile;
+        this.postImage = postImage;
+        this.like = like;
+        this.save = save;
+        this.likeList = likeList;
+        this.commentList = commentList;
+        this.time = time;
+        this.description = description;
+    }
+
+    public void setUserProfile(User userProfile) {
+        this.userProfile = userProfile;
+    }
+
+    public void setPostImage(String postImage) {
+        this.postImage = postImage;
+    }
+
+    public void setLike(boolean like) {
+        this.like = like;
+    }
+
+    public void setSave(boolean save) {
+        this.save = save;
+    }
+
+    public void setLikeList(List<User> likeList) {
+        this.likeList = likeList;
+    }
+
+    public void setCommentList(List<PostComment> commentList) {
+        this.commentList = commentList;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getUserProfile() {
+        return userProfile;
+    }
+
+    public String getPostImage() {
+        return postImage;
+    }
+
+    public boolean isLike() {
+        return like;
+    }
+
+    public boolean isSave() {
+        return save;
+    }
+
+    public List<User> getLikeList() {
+        return likeList;
+    }
+
+    public List<PostComment> getCommentList() {
+        return commentList;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
--- app/src/main/java/com/example/aragram/model/User.java
+++ app/src/main/java/com/example/aragram/model/User.java
@@ -1,143 +1,21 @@
 package com.example.aragram.model;
 
-import android.graphics.Bitmap;
+public class User {
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-
-public class User implements Serializable {
-    private String username;
-    private String password;
-    private String age;
-    private String gender;
-    private int id;
-    private int followers;
-    private int following;
-    private Bitmap profilePicture;
-
-    public String getUserProfilePicture() {
-        return userProfilePicture;
-    }
-
-    public void setUserProfilePicture(String userProfilePicture) {
-        this.userProfilePicture = userProfilePicture;
-    }
-
-    private String userProfilePicture;
-
-    public Bitmap getProfilePicture() {
-        return profilePicture;
-    }
-
-    public void setProfilePicture(Bitmap profilePicture) {
-        this.profilePicture = profilePicture;
-    }
-
-    public void setPosts(int posts) {
-        this.posts = posts;
-    }
-
-    private int posts;
-    private String bio;
-    private String website;
-
-    public String getWebsite() {
-        return website;
-    }
+    private String name;
 
     public User() {
     }
 
-    public void setWebsite(String website) {
-        this.website = website;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    List<User> followersUsers =new ArrayList<>();
-    List<User> followingUsers =new ArrayList<>();
-
-    public int getPosts() {
-        return posts;
+    public User(String name) {
+        this.name = name;
     }
 
-
-    public User(String username, String password, String age, String gender, String bio,String website) {
-        this.username = username;
-        this.password = password;
-        this.age = age;
-        this.gender = gender;
-        this.bio=bio;
-        followers=0;
-        following=0;
-        posts =0;
-
-    }
-
-    public String getBio() {
-        return bio;
-    }
-
-    public void setBio(String bio) {
-        this.bio = bio;
-    }
-
-    public List<User> getFollowersUsers() {
-        return followersUsers;
-    }
-
-    public List<User> getFollowingUsers() {
-        return followingUsers;
-    }
-
-    public int getFollowers() {
-        return followers;
-    }
-
-    public void setFollowers(int followers) {
-        this.followers = followers;
-    }
-
-    public int getFollowing() {
-        return following;
-    }
-
-    public void setFollowing(int following) {
-        this.following = following;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setAge(String age) {
-        this.age = age;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getAge() {
-        return age;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public int getId() {
-        return id;
+    public String getName() {
+        return name;
     }
 }
